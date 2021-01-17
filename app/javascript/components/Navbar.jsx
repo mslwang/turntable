5776ca803bb476fc6d8b4e0f0210005e9d739c20
Matthew Wang <mslwang2001@gmail.com>
2021-01-17 14:30:08 -0500

    Modify Navbar

--- app/javascript/components/Navbar.jsx
+++ app/javascript/components/Navbar.jsx
@@ -18,31 +18,13 @@ const NavigationBar = (props) => {
 
   const toggle = () => setIsOpen(!isOpen);
 
-  const logIn = () => {
-    const csrf = document.querySelector("meta[name='csrf-token']").getAttribute("content");
-    axios.get('/users/sign_in', { headers: {
-      'X-CSRF-Token': csrf,
-  }});
-  }
-
-  const logOut = async () => {
-    const csrf = document.querySelector("meta[name='csrf-token']").getAttribute("content");
-    const res = await axios.delete('/users/sign_out', { headers: {
-      'X-CSRF-Token': csrf,
-  }}).catch(err => {
-      console.log(err)
-    });
-  }
-
   return (
     <div>
       <Navbar color="light" light expand="md">
         <NavbarBrand href="/">turntable</NavbarBrand>
         <NavbarToggler onClick={toggle} />
-        <Collapse isOpen={isOpen} navbar>
 
-          {user ? <Container>
-
+          {user ? 
                 <Nav className="ml-auto" navbar> 
                 <NavLink href='/recipes/'>
                         Add Recipe
@@ -51,16 +33,12 @@ const NavigationBar = (props) => {
                     href='/users/sign_out' data-method="delete" rel="nofollow">
                   Logout
                   </NavLink>
-                </Nav>
-                </Container>: 
-                <Container>
-            <Nav>
+                </Nav>: 
+            <Nav className="ml-auto" navbar>
               <NavLink href="/users/sign_up">Sign Up</NavLink>
               <NavLink href="/users/sign_in">Log In</NavLink>
             </Nav>
-            </Container>
             }
-        </Collapse>
       </Navbar>
     </div>
   );
